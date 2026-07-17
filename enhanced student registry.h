0e62a93fbe37cef10e35c860ba02917f4f375801
DonovannYu <donovanndaye.yu.24@usjr.edu.ph>
2026-07-17 23:59:14 +0800

    Update and rename student registry.h to enhanced student registry.h

---
+++ enhanced student registry.h
@@ -0,0 +1,24 @@
+#ifndef STUDENT_REGISTRY_H
+#define STUDENT_REGISTRY_H
+
+#define MAX 10
+
+typedef struct
+{
+    int id;
+    char name[100];
+    char major[50];
+    float gpa;
+    int credits;
+} Student;
+
+void addStudent(Student students[], int *count);
+void displayAllStudents(Student students[], int count);
+void searchByID(Student students[], int count);
+void findByGPA(Student students[], int count);
+void findByMajor(Student students[], int count);
+
+void saveStudentsToFile(Student students[], int count);
+void loadStudentsFromFile(Student students[], int *count);
+
+#endif
